7ea624612b62ab006becdd2af1a2fa9d6068b9cf
loenko <loenko.js@gmail.com>
2019-09-01 20:32:26 +0500

    Styles for basic list colors and states

--- src/components/CurrencyListItem.vue
+++ src/components/CurrencyListItem.vue
@@ -3,7 +3,8 @@
         class="currency-list-item"
         v-bind:class="{
             'currency-list-item_featured': isItemOfFeaturedList,
-            'currency-list-item_basic': !isItemOfFeaturedList
+            'currency-list-item_basic': !isItemOfFeaturedList,
+            'currency-list-item_basic--inactive': !isItemOfFeaturedList && !currencyListItem.isSelected
         }"
     >
         <CurrencyListItemLabel
@@ -46,18 +47,25 @@ export default {
         margin: 1%;
         padding: 1%;
         display: flex;
-        background-color: #EEE;
         box-sizing: border-box;
     }
     .currency-list-item_featured {
         justify-content: center;
         font-size: 1.2rem;
         position: relative;
+        background-color: lightgrey;
     }
     .currency-list-item_basic {
         color: darkblue;
         justify-content: start;
         border: 1px solid grey;
         font-size: 1.5rem;
+        background-color: white;
+    }
+    .currency-list-item_basic--inactive {
+        background-color: #EEE;
+    }
+    .currency-list-item_basic--inactive:hover {
+        background-color: lightgrey;
     }
 </style>
