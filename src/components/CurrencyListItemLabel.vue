01d843d1837de5e863fe7ce445e965d2997653ac
loenko <loenko.js@gmail.com>
2019-09-01 23:53:17 +0500

    Customize checkbox

--- src/components/CurrencyListItemLabel.vue
+++ src/components/CurrencyListItemLabel.vue
@@ -47,5 +47,46 @@
 </script>
 
 <style scoped>
-
+    .currency-list-item-label {
+        width: 100%;
+        display: flex;
+        align-content: center;
+        justify-content: center;
+    }
+    .currency-list-item-label_checker {
+        width: 100%;
+        cursor: pointer;
+        position: relative;
+        padding-left: 22px;
+        user-select: none;
+    }
+    .currency-list-item-label_checker input {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+        height: 0;
+        width: 0;
+    }
+    .currency-list-item-checkmark {
+        position: absolute;
+        top: 19%;
+        left: 0;
+        height: 16px;
+        width: 16px;
+        border: 1px solid grey;
+        border-radius: 3px;
+    }
+    .currency-list-item-checkmark:after {
+        content: 'x';
+        line-height: 1.2rem;
+        font-size: 1.2rem;
+        margin-top: -2px;
+        color: red;
+        display: none;
+        align-content: center;
+        justify-content: center;
+    }
+    .currency-list-item-label_checker input:checked ~ .currency-list-item-checkmark:after {
+        display: flex;
+    }
 </style>
